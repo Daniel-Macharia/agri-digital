8c8b6e750f45e263f4c75d58f67743ae8c044c44
Daniel Macharia <ndungudanny444@gmail.com>
2025-05-31 12:56:09 +0300

    set icon paths relative to the root folder, not the src folder

--- src/pages/farmers-module/dashboard/components/side-bar/index.tsx
+++ src/pages/farmers-module/dashboard/components/side-bar/index.tsx
@@ -13,20 +13,20 @@ export default function SideBar()
             </div>
             <hr/>
             <div id="side-bar-items">
-                <SideBarItem name="Home" iconUrl ="../assets/home.svg"/>
-                <SideBarItem name="Projects" iconUrl ="../assets/journey.svg"/>
-                <SideBarItem name="My Products" iconUrl ="../assets/Group.svg"/>
-                <SideBarItem name="Market Place" iconUrl ="../assets/marketplace.svg"/>
-                <SideBarItem name="Banks" iconUrl ="../assets/bank.svg"/>
-                <SideBarItem name="Insurance" iconUrl ="../assets/bank.svg"/>
-                <SideBarItem name="Sponsors" iconUrl ="../assets/success.svg"/>
-                <SideBarItem name="Wallets" iconUrl ="../assets/wallet.svg"/>
-                <SideBarItem name="Packages" iconUrl ="../assets/package.svg"/>
-                <SideBarItem name="Quality Score" iconUrl ="../assets/quality_score.svg"/>
-                <SideBarItem name="Weather" iconUrl ="../assets/sunny.svg"/>
-                <SideBarItem name="Training & Resources" iconUrl ="../assets/articles.svg"/>
-                <SideBarItem name="Settings" iconUrl ="../assets/settings.svg"/>
-                <SideBarItem name="Invite a Friend" iconUrl ="../assets/invite.svg"/>
+                <SideBarItem name="Home" iconUrl ="../../assets/home.svg"/>
+                <SideBarItem name="Projects" iconUrl ="../../assets/journey.svg"/>
+                <SideBarItem name="My Products" iconUrl ="../../assets/Group.svg"/>
+                <SideBarItem name="Market Place" iconUrl ="../../assets/marketplace.svg"/>
+                <SideBarItem name="Banks" iconUrl ="../../assets/bank.svg"/>
+                <SideBarItem name="Insurance" iconUrl ="../../assets/bank.svg"/>
+                <SideBarItem name="Sponsors" iconUrl ="../../assets/success.svg"/>
+                <SideBarItem name="Wallets" iconUrl ="../../assets/wallet.svg"/>
+                <SideBarItem name="Packages" iconUrl ="../../assets/package.svg"/>
+                <SideBarItem name="Quality Score" iconUrl ="../../assets/quality_score.svg"/>
+                <SideBarItem name="Weather" iconUrl ="../../assets/sunny.svg"/>
+                <SideBarItem name="Training & Resources" iconUrl ="../../assets/articles.svg"/>
+                <SideBarItem name="Settings" iconUrl ="../../assets/settings.svg"/>
+                <SideBarItem name="Invite a Friend" iconUrl ="../../assets/invite.svg"/>
             </div>
 
             <div id="assistance-div">
