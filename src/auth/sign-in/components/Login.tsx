ef3744599971cbb916ba430c85d1d2eed90af889
Daniel Macharia <ndungudanny444@gmail.com>
2025-07-15 20:38:10 +0300

    remove custom styles

--- src/auth/sign-in/components/Login.tsx
+++ src/auth/sign-in/components/Login.tsx
@@ -3,12 +3,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 import * as Yup from 'yup';
 
- import "./Login.css";
- import "../../style.css";
-
 import { Formik, Form, Field, ErrorMessage} from "formik";
 import sendOtpUtil from "../utils/SendOtpUtil.ts";
 import { storagePut } from "../utils/StorageUtils.ts";
+
+import "../../index.css";
 
 
 const Login: React.FC = () => {
@@ -52,92 +51,111 @@ const Login: React.FC = () => {
         return (
             <>
                 {/* <div> */}
-                    <div id='sign-up-content-div' >
-                        <div id='sign-up-form-div' >
-                            <p id='sign-up-form-title'>Login to your account.</p>
-                            <p id='sign-up-form-sub-title'>Join the Future of Farming - Easy, Fast and Reliable.</p>
+                    <div className="col-12" style={{backgroundColor: "var(--Background, #F5F5F5)"}}>
+                        <div className="row justify-content-center align-items-center">
+                            <div className="col-4" >
+                                <div className="col-12">
+                                    <p className="h1-bold primary-text mb-0">
+                                        Login to your account.
+                                    </p>
+                                    <p className=" body-regular primary-text">
+                                        Join the Future of Farming - Easy, Fast and Reliable.
+                                    </p>
+                                </div>
 
-                            <Formik 
-                            
-                            initialValues={initialValues}
-                            validationSchema={validationSchema}
-                            onSubmit={handleLogin}
-                            >
-                                {
-                                    ({}) =>(
+                                <div className="col-12">
+                                    <Formik 
+                                    
+                                    initialValues={initialValues}
+                                    validationSchema={validationSchema}
+                                    onSubmit={handleLogin}
+                                    >
+                                        {
+                                            ({}) =>(
 
 
-                                        <Form className='sign-up-form'>
-                                            <label className="input-label" htmlFor='email'>Email address/Phone number *</label>
-                                            
-                                            <div className="text-danger small">
-                                                <ErrorMessage name="userEmail" />
-                                            </div>
-                                            <Field 
-                                            name="userEmail" 
-                                            className='input-field' 
-                                            type='text' 
-                                            id='email' 
-                                            placeholder='example@gmail.com/+254712345678' />
+                                                <Form className='col-12' >
+                                                    <label className="body-regular primary-text mb-0" htmlFor='email'>
+                                                        Email address/Phone number *
+                                                    </label>
+                                                    
+                                                    <div className="col-12 mb-2">
+                                                        <Field 
+                                                        name="userEmail" 
+                                                        className='form-control body-regular' 
+                                                        type='text' 
+                                                        id='email' 
+                                                        placeholder='example@gmail.com/+254712345678' />
+                                                        <div className="text-danger small my-0">
+                                                            <ErrorMessage name="userEmail" />
+                                                        </div>
+                                                    </div>
 
-                                            <button 
+                                                    <div className="col-12">
+                                                        <button 
+                                                        type='submit' 
+                                                        name="pass"
+                                                        className="col-12 mx-0 auth-accept-button"
+                                                        onClick={ ()=>{ setSubmitType("pass") } }
+                                                        >
+                                                            Login with password
+                                                        </button>
+                                                    </div>
 
-                                            type='submit' 
-                                            name="pass" 
-                                            id="create-account-button" 
-                                            value={"pass"}
+                                                    <div className="col-12 px-0 mx-0 my-3" >
+                                                        <div className="row px-0 mx-0 justify-content-center align-items-center">
+                                                            <div className="col-4 mx-0 px-0" >
+                                                                <hr className="mx-0 col-12" />
+                                                            </div>
+                                                            <div className="col-1 mx-0 px-0" >
+                                                                <p className="mx-0 col-12 auth-center-aligned-text" >
+                                                                    or
+                                                                </p>
+                                                            </div>
+                                                            <div className="col-4 mx-0 px-0" >
+                                                                <hr className="mx-0 col-12" />
+                                                            </div>
+                                                        </div>
+                                                    </div>
 
-                                            onClick={ ()=>{ setSubmitType("pass") } }
-                                            >
-                                                Login with password
-                                            </button>
+                                                    <div className="col-12">
+                                                        <button 
+                                                        type="submit"
+                                                        name="otp" 
+                                                        className="col-12 auth-white-button"
+                                                        onClick={ ()=>{setSubmitType("otp")} }
+                                                        >
+                                                            Login with O.T.P
+                                                        </button>
+                                                    </div>
+                                                </Form>
 
-                                            <span id="horizontal-rule-span">
-                                                <hr className="horizontal-rule"/>
-                                                <p>or</p>
-                                                <hr className="horizontal-rule"/>
+                                            )
+                                        }
+                                        
+                                    </Formik>
+
+                                </div>
+
+                                <div className="col-12">
+                                    <div
+                                    className="col-12" 
+                                    onClick={() => {navigate("/auth/sign-up");}}>
+                                        <p className='auth-center-aligned-text'>
+                                            Do not have an account ? {" "} 
+                                            <span className='login-button'>
+                                                Sign up
                                             </span>
+                                        </p>
+                                    </div>
+                                </div> 
+                            </div>
 
-                                            <button 
-
-                                            type="submit"
-                                            name="otp" 
-                                            id="login-with-otp-button" 
-                                            value={"otp"}
-
-                                            onClick={ ()=>{setSubmitType("otp")} }
-                                            >
-                                                Login with O.T.P
-                                            </button>
-                                        </Form>
-
-                                    )
-                                }
-                                
-                            </Formik>
-                            {/* <form id='sign-up-form' >
-                                <label className="input-label" htmlFor='email'>Email address/Phone number *</label>
-                                <input name="phone-or-email" className='input-field' type='text' id='email' placeholder='example@gmail.com/+254712345678' />
-
-                                <input type='submit' name="create-button" id="create-account-button" value={"Login with password"}/>
-                                
-                                <span id="horizontal-rule-span">
-                                    <hr className="horizontal-rule"/>
-                                    <p>or</p>
-                                    <hr className="horizontal-rule"/>
-                                </span>
-
-                                <input type='submit' name="login-with-otp-button" id="login-with-otp-button" value={"Login with O.T.P"}/>
-                            
-                            </form> */}
-
-                            <div id='login-div'>
-                                <Link to="/auth/sign-up">
-                                <label id='login-label'>Do not have an account ?  <span id='login-button'>Sign up</span></label>
-                                </Link>
-                            </div> 
+                            <div className="col-6" style={{backgroundColor: "purple"}}>
+                                <img src='/shamba_bot_logo.svg' alt='logo'
+                                style={{width: "100%"}}/>
+                            </div>
                         </div>
-                        <img src='/shamba_bot_logo.svg' alt='image here'/>
                     </div>
                 {/* </div > */}
             </>
