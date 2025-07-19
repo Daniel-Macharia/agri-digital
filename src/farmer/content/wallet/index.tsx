e3756c089bad28d140ac394840aed5ac522c6a6a
Billy <osayebilly98@gmail.com>
2025-07-19 21:33:40 +0300

    Beneficiaries

--- src/farmer/content/wallet/index.tsx
+++ src/farmer/content/wallet/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import TransactionHistory from "./Components/TransactionHistory";
 import Topup from "./Components/Topup";
 import Vouchers from "./Components/Vouchers";
+import Beneficiaries from "./Components/Beneficiaries";
 
 const WalletRoutes = () => {
   return (
@@ -9,6 +10,8 @@ const WalletRoutes = () => {
       <Route path="" element={<TransactionHistory />} />
       <Route path="topup" element={<Topup />} />
       <Route path="vouchers" element={<Vouchers />} />
+      <Route path="beneficiaries" element={<Beneficiaries />} />
+
     </Routes>
   );
 };
