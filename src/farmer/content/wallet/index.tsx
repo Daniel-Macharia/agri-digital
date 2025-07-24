ccc3dfe3f371b58b540b7785317653a1b0444954
Billy <osayebilly98@gmail.com>
2025-07-24 12:34:06 +0300

    before build

--- src/farmer/content/wallet/index.tsx
+++ src/farmer/content/wallet/index.tsx
@@ -4,15 +4,30 @@ import Topup from "./Components/Topup";
 import Vouchers from "./Components/Vouchers";
 import Beneficiaries from "./Components/Beneficiaries";
 import CreatingVoucher from "./Components/CreatingVoucher";
+import { WALLET_ROUTES } from "./Components/Wallet.Route";
+
+
 
 const WalletRoutes = () => {
   return (
     <Routes>
-      <Route path="" element={<TransactionHistory />} />
-      <Route path="topup" element={<Topup />} />
-      <Route path="vouchers" element={<Vouchers />} />
-      <Route path="beneficiaries" element={<Beneficiaries />} />
-      <Route path="creating" element={<CreatingVoucher />} />
+     
+
+
+      <Route path={WALLET_ROUTES.LANDING} element={<TransactionHistory />} />
+      <Route path={WALLET_ROUTES.TOPUP} element={<Topup />} />
+      <Route path={WALLET_ROUTES.VOUCHERS} element={<Vouchers />} />
+      <Route path={WALLET_ROUTES.BENEFICIARIES} element={<Beneficiaries />} />
+      <Route path={WALLET_ROUTES.CREATING} element={<CreatingVoucher />} />
+
+
+
+     
+
+
+
+
+
     </Routes>
   );
 };
