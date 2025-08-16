541bd9b3aa0eb27d56e20e1f5d935ae8ffacdf25
Billy <osayebilly98@gmail.com>
2025-08-17 02:03:38 +0300

    remove unnecessary files from patners home

--- src/patners/content/home/home-notifications/home-enums.ts
+++
@@ -1,5 +0,0 @@
-export enum HomeNotificationType{
-    GENERAL_NOTIFICATION = "other",
-    SPONSORSHIP_NOTIFICATION = "sponsorship",
-    ORDER_NOTIFICATION = "order"
-};
--- src/patners/content/home/home-routes.ts
+++ src/patners/content/home/home-routes.ts
@@ -1,11 +1,7 @@
 import { PATNERS_ROUTES } from "../../patners-routes";
 
 export const PATNERS_HOME_ROUTES = {
-    HOME_NOTIFICATIONS: "/notifications",
-    HOME_TASKS: "/tasks",
-    HOME_ORDERS: "/orders",
-    HOME: "",
-    HOME_TRIAL: "/trial",
+
 
     HOME_FULL: `/patners${PATNERS_ROUTES.HOME.replace("/*", "")}`,
 };
